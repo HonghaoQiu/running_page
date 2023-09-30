9802f241051dafdcedd4d0b0893294c636582862
HonghaoQiu <2624577447@qq.com>
2023-10-01 02:12:10 +0800

    Run

---
+++ src/static/site-metadata.ts
@@ -0,0 +1,29 @@
+interface ISiteMetadataResult {
+  siteTitle: string;
+  siteUrl: string;
+  description: string;
+  logo: string;
+  navLinks: {
+    name: string;
+    url: string;
+  }[];
+}
+
+const data: ISiteMetadataResult = {
+  siteTitle: 'Running Page',
+  siteUrl: 'https://qhh.run',
+  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+  description: 'Personal site and blog',
+  navLinks: [
+    {
+      name: 'Blog',
+      url: 'https://qhh.run',
+    },
+    {
+      name: 'About',
+      url: 'https://qhh.run',
+    },
+  ],
+};
+
+export default data;
