b86193b7f0e62170d59caeac57736f6945c2cd66
HonghaoQiu <73233908+HonghaoQiu@users.noreply.github.com>
2023-10-01 20:24:58 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,17 +11,17 @@ interface ISiteMetadataResult {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Running Page',
-  siteUrl: 'https://qhh.run',
+  siteUrl: 'https://honghaoqiu.github.io/running_page/',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
   description: 'Personal site and blog',
   navLinks: [
     {
       name: 'Blog',
-      url: 'https://qhh.run',
+      url: 'https://github.com/HonghaoQiu',
     },
     {
       name: 'About',
-      url: 'https://qhh.run',
+      url: 'https://github.com/HonghaoQiu',
     },
   ],
 };
